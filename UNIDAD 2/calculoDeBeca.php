ad36052f1fafeb3fb8579b913953754a3c09da3b
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-03-06 13:03:47 +0100

    barbaridades varias

--- UNIDAD 2/calculoDeBeca.php
+++ UNIDAD 2/calculoDeBeca.php
@@ -2,11 +2,11 @@
 
 $promedio = 8.7;
 
-if ($promedio <= 10 && $promedio >= 9) {
+if ($promedio >= 9) {
     echo "Tienes beca de 50%<br>";    
-} elseif ($promedio < 9 && $promedio >= 8) {
+} elseif ($promedio >= 8) {
     echo "Tienes beca de 30%<br>";
-} elseif ($promedio < 8 && $promedio >= 7) {
+} elseif ($promedio >= 7) {
     echo "Tienes beca de 10%<br>";
 } else {
     echo "No tienes beca<br>";
