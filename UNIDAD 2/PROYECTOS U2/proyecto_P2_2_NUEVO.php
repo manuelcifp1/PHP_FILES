e1dd5a86691441aa724d6a2ac5c37ac5c30e525e
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-03-08 15:00:30 +0100

    Proyecto P2_2 nuevecito

--- UNIDAD 2/PROYECTOS U2/proyecto_P2_2_NUEVO.php
+++ UNIDAD 2/PROYECTOS U2/proyecto_P2_2_NUEVO.php
@@ -17,26 +17,40 @@ $productos = [
     ["nombre"=>"pantalla", "cantidad"=>30, "precio"=>120],
     ["nombre"=>"raton", "cantidad"=>8, "precio"=>12]
 ];
+/*MUY IMPORTANTE: si no ponemos & delante de $productos o $producto no funcionaran los cambios, porque estaríamos trabajando
+con una copia del array productos y una copia de producto. Al trabajar por referencia (&), actuamos sobre el array original
+(directamente en su espacio de memoria), lo cual es vital en este ejercicio para poder actualizar el inventario.*/
 
-function actualizarInventario (array &$productos, string $nombreNuevo, int $cantidadNueva, int $precioNuevo): array {
-    foreach($productos as ["nombre"=>$valorNombre, "cantidad"=>$valorCantidad]) {
-        if ($valorNombre == $nombreNuevo) {
-            $valorCantidad+= $cantidadNueva;
+/*En la función siguiente, tipamos adecuadamente a los parámetros y, despues de los :, tipamos al return de la función que será el array
+modificado. */
+function actualizarInventario (array &$productos, string $nombre, int $cantidad, int $precio): array {
+    foreach($productos as &$producto) {
+        //Si el nombre del nuevo producto es igual a alguno de los que ya existe, aumentamos el número de unidades.
+        if ($producto["nombre"] === $nombre) {
+            $producto["cantidad"] += $cantidad;
+            break;
         } else {
-            $productos[] = ["nombre"=>$nombreNuevo, "cantidad"=>$cantidadNueva, "precio"=>$precioNuevo];
-        }
+            //En caso contrario, añadimos el nuevo producto al inventario
+            $productos[] = ["nombre"=>$nombre, "cantidad"=>$cantidad, "precio"=>$precio];
+            break;
+        }        
     }
+    //Devuelve el array modificado.
     return $productos;
 }
 
+/*Recorremos el array asociativo con un foreach para mostrarlo correctamente.*/
 function mostrarInventario (array $productos) {
     foreach ($productos as ["nombre"=>$valorNombre, "cantidad"=>$valorCantidad, "precio"=>$valorPrecio]) {
         echo "Nombre: $valorNombre, Cantidad: $valorCantidad, Precio: $valorPrecio" . "<br>";
     }    
 }
-
+//Llamada a actualizarInventario con un nuevo producto a añadir.
 actualizarInventario($productos, "joystick", 23, 37);
+
+//Llamada a actualizarInventario con un producto existente para aumentar el número de unidades (cantidad).
 actualizarInventario($productos, "teclado", 10, 27);
 
+//Llamada a mostrarInventario para que muestre en pantalla el inventario de productos.
 mostrarInventario($productos);
 
