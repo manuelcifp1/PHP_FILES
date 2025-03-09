b4790448740489bfe5def58496577cf903d7fe58
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-03-09 20:30:34 +0100

    resumen

---
+++ UNIDAD 2/FUNCIONES PHP.php
@@ -0,0 +1,30 @@
+<?php
+
+print_r($array);//Imprime array
+
+$array[] = 10;//Agrega el 10 al final del array
+
+array_push($array, 3,7);//AGREGA los values AL FINAL del array (Se usa para agregar VARIOS valores)
+
+unset($array[$i]);//ELIMINA el elemento con índice i.
+
+array_pop($array);//ELIMINA el ÚLTIMO elemento.
+
+array_shift($array);//ELIMINA el PRIMER elemento.
+
+sort($array);//ORDENA los valores del array en orden ascendente ROMPIENDO el par CLAVE=>VALOR.
+
+asort($array);//ORDENA los valores del array. MANTIENE el par CLAVE=>VALOR.
+
+ksort($array);//ORDENA un array asociativo por la CLAVE de cada valor
+
+preg_replace("/[^a-z]/", "", $cadena);/*Eliminar espacios y signos de puntuación
+ (regEx: todo lo que no sea una minúscula se reemplaza por nada "")
+ preg_replace reemplaza fragmentos de cadena equivalentes a una regEx por un replacement.*/
+
+//Eliminar acentos.
+$acentos = ["á"=>"a", "é"=>"e","í"=>"i", "ó"=>"o", "ú"=>"u"];
+strtr($cadena, $acentos); //strtr reemplaza elementos de una cadena usando un array...
+strtr("Hilla Warld","ia","eo");//...o también con strings (la i por e, la a por o)
+
+strrev($cadena);//Invertir cadena
--- UNIDAD 2/actividad_A2_9_NUEVO.php
+++ UNIDAD 2/actividad_A2_9_NUEVO.php
@@ -20,30 +20,60 @@ Supongamos que tienes un array que almacena una lista de números enteros. Neces
 
 /*2. Comprobar si una cadena es un palíndromo: Crear una función que verifique si una cadena es un palíndromo
  (se lee igual de izquierda a derecha que de derecha a izquierda, ignorando espacios, mayúsculas y acentos).*/
-function comprobarPalindromo(string $cadena): string {
-    //Quitamos espacios y cambiamos a minúsculas
-    $cadena = trim(string: strtolower(string: $cadena));
+function comprobarPalindromo(string $cadena) {
+    //Cambiamos a minúsculas
+    $cadena = strtolower( $cadena);
+    echo "Cambio a minúsculas: $cadena <br>";//Creamos puntos de control para verificar que todo va bien.    
     //Eliminamos acentos
     $acentos = ["á"=>"a", "é"=>"e","í"=>"i", "ó"=>"o", "ú"=>"u"];
-    $sinAcentos = strtr($cadena, $acentos);
+    $cadena = strtr($cadena, $acentos);
+    echo "Eliminación de acentos: $cadena <br>";
+    /*Eliminamos espacios y signos de puntuación (expresión regular que representa
+    todo lo que no sea una minúscula se cambia por nada "")*/
+    $cadena = preg_replace("/[^a-z]/", "", $cadena);
+    echo "Eliminación de espacios y signos de puntuación: $cadena <br>";
     //Invertimos la cadena
-    $invertida = strrev($sinAcentos);
+    $invertida = strrev($cadena);
+    echo "Invertimos la cadena: $invertida <br>";
     //Comprobamos si la inversión es igual a la cadena original
-    if ($invertida == $cadena) {
+    if ($invertida === $cadena) {
         echo "La cadena es palíndromo<br>";
     } else {
         echo "La cadena no es palíndromo<br>";
     }    
 };
 
-comprobarPalindromo("lámina");
-
+comprobarPalindromo("Dábale arroz a la zorra el abad");
 
 /*3. Reemplazar una palabra en una cadena: Crear una función que reciba una cadena, una palabra a buscar y una palabra de reemplazo,
  y reemplace todas las ocurrencias de la palabra buscada por la palabra de reemplazo.*/
+function reemplazarPalabra ($palabraBuscar, $palabraReemplazo, $string) {
+    //De nuevo, usamos preg_replace con una regEx para hacer el cambio de palabras
+    $reemplazada = preg_replace("/$palabraBuscar/", $palabraReemplazo, $string);
+    echo "$reemplazada<br>";
+} 
+
+reemplazarPalabra( "Manolo", "Pepe", "Me llamo Manolo");
 
 /*4. Obtener la fecha y hora actual: Crear una función que devuelva la fecha y hora actual en el formato Y-m-d H:i:s.*/
+function fechaHoraActual() {
+    echo date("Y-m-d H:i:s<br>");
+}
+
+fechaHoraActual();
+echo "<br>";
 
 /*5. Sumar días a una fecha: Crear una función que reciba una fecha y una cantidad de días,
  y devuelva la nueva fecha después de sumar esos días.*/
+function sumaDias($fecha, $numeroDias) {
+    //Usamos strtotime, función con la que se pueden modificar fechas fácilmente
+    $nuevaFecha = date("Y-m-d", strtotime($fecha . " +$numeroDias days"));
 
+echo "$nuevaFecha<br>";
+
+}
+
+sumaDias("2025-03-09", 863);
+
+
+ ?>
