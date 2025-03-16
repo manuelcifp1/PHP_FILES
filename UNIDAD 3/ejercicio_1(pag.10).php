d329ad2c924886f15a552fb6a2d2001484650b29
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-03-16 19:55:14 +0100

    validaciones

--- UNIDAD 3/ejercicio_1(pag.10).php
+++ UNIDAD 3/ejercicio_1(pag.10).php
@@ -8,13 +8,13 @@
 <body>
     <form action="#" method="get">
         <label for="nombre">Nombre: </label>
-        <input type="text" name="nombre" id="nombre" <?php if(!empty($_REQUEST['nombre'])):?> value="<?php echo htmlspecialchars($_REQUEST['nombre']);?>" <?php endif;?> required>
+        <input type="text" name="nombre" id="nombre" <?php if(!empty($_REQUEST['nombre'])):?> value="<?php echo htmlspecialchars($_REQUEST['nombre'], ENT_QUOTES, 'UTF-8');?>" <?php endif;?> required>
 
         <label for="email">Email: </label>
-        <input type="email" name="email" id="email" <?php if(!empty($_REQUEST['email'])):?> value="<?php echo htmlspecialchars($_REQUEST['email']);?>" <?php endif;?> required>
+        <input type="email" name="email" id="email" <?php if(!empty($_REQUEST['email'])):?> value="<?php echo htmlspecialchars($_REQUEST['email'], ENT_QUOTES, 'UTF-8');?>" <?php endif;?> required>
 
         <label for="apellido">Apellido: </label>
-        <input type="text" name="apellido" id="apellido" <?php if(!empty($_REQUEST["apellido"])):?> value="<?php echo htmlspecialchars($_REQUEST['apellido']);?>" <?php endif;?> required>    
+        <input type="text" name="apellido" id="apellido" <?php if(!empty($_REQUEST["apellido"])):?> value="<?php echo htmlspecialchars($_REQUEST['apellido'], ENT_QUOTES, 'UTF-8');?>" <?php endif;?> required>    
 
         <label for="comentario">Comentario: </label>
         <textarea name="comentario" id="comentario"></textarea>
@@ -22,7 +22,7 @@
         <button type="submit">Enviar</button>
     </form>
     <?php
-        if(isset ($_REQUEST['nombre']) && isset($_REQUEST['email'])  && isset($_REQUEST['apellido']) && isset($_REQUEST['comentario'])) {
+        if(!empty ($_REQUEST['nombre']) && !empty($_REQUEST['email'])  && !empty($_REQUEST['apellido']) && !empty($_REQUEST['comentario'])) {
             $nombre = $_REQUEST['nombre'];
             echo "<p>Gracias, $nombre. Hemos recibido tu comentario</p>";
         }           
