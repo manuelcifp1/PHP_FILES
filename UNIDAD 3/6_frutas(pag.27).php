acbee27efa2ffaa7ae70d48adeced03cace62872
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-03-17 17:30:55 +0100

    cambio nombres

--- UNIDAD 3/6_frutas(pag.27).php
+++ UNIDAD 3/6_frutas(pag.27).php
@@ -5,13 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FRUTAS EN ARRAY</title>
 </head>
-<!--Presentaré un formulario donde preguntaré ¿Cuántas frutas deseas indicar?
+<!--Presentar un formulario donde preguntar: ¿Cuántas frutas deseas indicar?
 ● Cuando el usuario me ha indicado el número de frutas, simplemente
 crearemos un formulario dinámico con una serie de campos input, tantos
 como frutas quería indicar.
 ● Por último haremos un recorrido para recibir todas las frutas y mostrarlas
 en la página.-->
 <body>
+    <form action="#" method="get">
+        <label for="cuantas">¿Cuantas frutas quieres introducir?</label>
+        <input type="number" name="cuantas" id="cuantas" required>
+        <button type="submit">ENVIAR</button>
+    </form>
+    <?php
+
+    if(!empty($_GET['cuantas'])) {
+        $cuantas = htmlspecialchars($_GET['cuantas'], ENT_QUOTES, 'UTF-8');
+
+        
+    }
+
+    ?>
     
 </body>
 </html>
