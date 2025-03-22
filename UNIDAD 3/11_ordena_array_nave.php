b8491ba8522d3c627e71edb3aa80cc8de8ac59e7
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-03-22 21:56:31 +0100

    varios

--- UNIDAD 3/11_ordena_array_nave.php
+++ UNIDAD 3/11_ordena_array_nave.php
@@ -1,8 +1,12 @@
 <?php
 
+$numbers = [95, 80, 60, 75, 90, 30, 5, 13, 25];
+
 usort($numbers, function($a, $b) {
     return $a <=> $b;
 });
+
+print_r($numbers);
 /*Esto le dice a usort():
 
 Si $a < $b, devuelve -1 → $a va antes que $b
--- UNIDAD 3/actividad_A3_1.php
+++ UNIDAD 3/actividad_A3_1.php
@@ -6,10 +6,30 @@ Supongamos que tienes una lista de calificaciones de estudiantes y deseas catego
   la calificación de cada estudiante y asignarles una categoría basada en si son mejores,
    peores o iguales a ciertos umbrales. */
 
-$grades = [95, 80, 60, 75, 90];
+$grades = [95, 80, 60, 75, 90, 30];
+$suspenso = [];
+$aprobado = [];
+$notable = [];
+$sobresaliente = [];
 
+foreach($grades as $grade) {
+    if (($grade<=>89) == 1) {
+        $sobresaliente[] = $grade;
+    } elseif(($grade<=>69) == 1) {
+        $notable[] = $grade;
+    } elseif(($grade<=>49) == 1) {
+        $aprobado[] = $grade;
+    } elseif(($grade<=>0) == 1) {
+        $suspenso[] = $grade;
+    }
+}
 
+print_r($suspenso)  . "<br>";
+print_r($aprobado) . "<br>";
+print_r($notable) . "<br>";
+print_r($sobresaliente)  . "<br>";
 
+echo "<br><br>";
 /*Clasificación de productos por volumen de ventas
 
 Si estamos desarrollando un sitio de ventas, podemos utilizar el operador <=> para clasificar
@@ -27,6 +47,8 @@ $products = [
 ];
 
 
+
+
 /*Ordenar entrega de paquetes por distancia.
 
 Supongamos que estás desarrollando una aplicación de entrega de pedidos y deseas determinar
@@ -35,6 +57,13 @@ Supongamos que estás desarrollando una aplicación de entrega de pedidos y dese
 
 $distances = [5.2, 3.8, 7.1, 2.5];
 
+usort($distances, function($distanceA, $distanceB) {
+     return $distanceA<=>$distanceB;
+    });
+
+    print_r($distances);
+
+    echo "<br><br>";
 
 /*Clasificación de productos según reseñas
 
@@ -52,27 +81,62 @@ $productos = [
 
 ];
 
+foreach($productos as ["rating"=> $rating]) {
+    switch($rating) {
+        case ($rating<=>4.8) === 0:
+        echo "<p>Producto C bien calificado.<br>";
+        break;
 
+        case ($rating<=>4.5) === 0:
+        echo "<p>Producto A regular calificado.<br>";
+        break;
+
+        case ($rating<=>3.2) === 0:
+        echo "<p>Producto B mal calificado.<br>";
+        break;
+
+    }
+}
+
+echo "<br><br>";
 /*Dado un array de edades. Utiliza usort con una función flecha para ordenar el array de
  menor a mayor usando el operador de nave espacial.*/
  $edades = [25, 18, 32, 20, 40, 27];
 
+ usort($edades, fn ($edadA, $edadB) =>  $edadA<=>$edadB);
 
+ print_r($edades);
+
+ echo "<br><br>";
 /*Dado un array de nombres, usa usort y una función flecha para ordenar los nombres de forma
  descendente (de Z a A) alfabéticamente.*/ 
  $nombres = ["Ana", "Carlos", "Beatriz", "Eduardo", "Daniel"];
 
+ usort($nombres, fn ($nomA, $nomB) => $nomB<=>$nomA);
 
+ print_r($nombres);
+
+ echo "<br><br>";
 /*Tienes un array de precios de productos. Utiliza usort con una función flecha para ordenar
  los precios de mayor a menor. */
   $precios = [200.5, 99.99, 150.0, 50.75, 120.25];
+
+  usort($precios, fn ($precA, $precB) => $precB<=>$precA);
+
+    print_r($precios);
+
+    echo "<br><br>";
 
 
 /*Dado un array de nombres de ciudades, usa usort y una función flecha para ordenar las ciudades
  de menor a mayor según la longitud de sus nombres.*/
   $ciudades = ["Roma", "Santiago", "Buenos Aires", "Lima", "La Paz", "Montevideo"];
 
+  usort($ciudades, fn ($cityA, $cityB) => ($cityA.length)<=>$cityB.length);
 
+  print_r($precios);
+ 
+  echo "<br><br>";
 /*Tienes un array que contiene números negativos y positivos. Usa usort con una función flecha
  y el operador de nave espacial para ordenarlos de menor a mayor.*/
  $numeros = [3, -1, -4, 2, 0, -2, 5];
