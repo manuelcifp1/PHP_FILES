73aa5ad0ef05a45f7e52c03a50d05ff87f3d893c
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-05-02 21:37:43 +0200

    class

--- UNIDAD 5/ACTIVIDADES/A5_3/index1.php
+++ UNIDAD 5/ACTIVIDADES/A5_3/index1.php
@@ -8,37 +8,18 @@ class Validador {
 
     public static function validarNombre($nombre) {
         $regExpNombre = "/^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ´' ]+$/";
-        if(!preg_match($regExpNombre, $nombre)) {
-            echo "<p>Nombre inválido</p>";
-        }
-
-       return $nombre = htmlspecialchars($nombre);
+        return preg_match($regExpNombre, $nombre);       
     }
 
-    public static function validarApellido($apellido) {
-        $regExpApellido = "/^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ´' ]+$/";
-        if(!preg_match($regExpApellido, $apellido)) {
-            echo "<p>Apellido inválido</p>";
-        }
-
-        return $apellido = htmlspecialchars($apellido);
-    }
-
-    public static function validarEmail($email) {
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo "<p>Email no válido</p>";
-        }
-        return $email = htmlspecialchars($email);
-    }
-
-    public static function validarPassword($password) {
-        $regExpPassword = "/[0-9]{4}/";
-        if(!preg_match($regExpPassword, $password)) {
-            echo "<p>Apellido inválido</p>";
-        }
-
-        return $password;
-    }
+    /* En el código:
+    if(!Validador::validarNombre($nombre)) {
+        echo "Nombre no válido";    
+    } else {
+        $nombre = htmlspecialchars(trim($nombre)); 
+    } */
+    
+    
+   
 
 }
 ?>
---
+++ UNIDAD 5/ACTIVIDADES/A5_4/cuentaBancaria.php
@@ -0,0 +1,17 @@
+<?php
+/*Crea una clase llamada CuentaBancaria con propiedades privadas $titular (nombre del titular)
+ y $saldo (saldo de la cuenta).
+- La clase CuentaBancaria debe tener métodos ingresar($cantidad), retirar($cantidad) y mostrarSaldo()
+ para gestionar los fondos de la cuenta.
+- Crea una clase llamada CuentaAhorro que herede de CuentaBancaria y tenga una propiedad protegida
+ $tasaInteres, que será el porcentaje de interés anual de la cuenta de ahorro.
+- La clase CuentaAhorro debe tener un método calcularIntereses() que calcule el interés ganado sobre
+ el saldo de la cuenta de ahorro, basado en la tasa de interés.
+- En el script principal, crea una instancia de CuentaBancaria y una instancia de CuentaAhorro,
+ luego llama a los métodos correspondientes para realizar ingresos, retiros y mostrar la información de la cuenta. */
+
+
+class CuentaBancaria {
+    
+} 
+?>
