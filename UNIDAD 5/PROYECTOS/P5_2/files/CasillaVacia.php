9f5c09a3ba633e871074f71e95e73de984eede5d
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-05-07 13:25:44 +0200

    notas finales

--- UNIDAD 5/PROYECTOS/P5_2/files/CasillaVacia.php
+++ UNIDAD 5/PROYECTOS/P5_2/files/CasillaVacia.php
@@ -1,14 +1,18 @@
 <?php
 /*Aquí creamos la clase CasillaVacia para gestionar la casilla que
- se queda vacía tras un movimiento*/
+ se queda vacía tras un movimiento.
+ Me gustaría mencionar que los métodos que CasillaVacia está obligada a definir
+ como hija de Pieza tienen el cuerpo vacío ¿Por qué? Porque esta clase no necesita usarlos,
+ pero al definirlos así, cumple perfectamente el "contrato de definición abstracto" que tiene
+ con su papi. ¿Curioso verdad? */
 require_once "Pieza.php";
 
 class CasillaVacia extends Pieza {
     public function mover(string $posicion): void {
-        // No hace nada
+        //No hace nada
     }
 
     public function capturar(): void {
-        // No hace nada
+        //No hace nada
     }
 }
