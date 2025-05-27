1cf8d0a6555b278af5601193174af0f87a0ef977
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-05-27 12:17:29 +0200

    ea

--- UNIDAD 6/P6_2/model/usuario.php
+++ UNIDAD 6/P6_2/model/usuario.php
@@ -1,26 +1,23 @@
 <?php
 //Para la tabla usuario, sólo necesitamos el método create, que usaremos cuando un nuevo usuario se registre.
 
-class Usuario
-{
+class Usuario {
+    
     private $conn;
     private $table = 'usuarios';
 
-    public function __construct($db)
-    {
+    public function __construct($db) {
         $this->conn = $db;
     }    
 
-    public function create($nombre, $password)
-{
-    $rol = 'cliente'; //fijo
-    $query = "INSERT INTO {$this->table} (nombre, password, rol) VALUES (:nombre, :password, :rol)";
-    $stmt = $this->conn->prepare($query);
-    $stmt->bindParam(':nombre', $nombre);
-    $stmt->bindParam(':password', $password);
-    $stmt->bindParam(':rol', $rol);
-    return $stmt->execute();
-}
-   
+    public function create($nombre, $password) {
 
+        $rol = 'cliente'; //fijo
+        $query = "INSERT INTO {$this->table} (nombre, password, rol) VALUES (:nombre, :password, :rol)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':nombre', $nombre);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':rol', $rol);
+        return $stmt->execute();
+    }
 }
