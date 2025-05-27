ab96b64cda08285c35eed5285b41b84b4df8ec50
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-05-27 10:53:18 +0200

    ea

--- UNIDAD 6/P6_2/model/usuario.php
+++ UNIDAD 6/P6_2/model/usuario.php
@@ -1,4 +1,5 @@
 <?php
+//Para la tabla usuario, sólo necesitamos el método create, que usaremos cuando un nuevo usuario se registre.
 
 class Usuario
 {
@@ -8,42 +9,18 @@ class Usuario
     public function __construct($db)
     {
         $this->conn = $db;
-    }
+    }    
 
-    public function read()
-    {
-        $query = "SELECT * FROM {$this->table}";
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+    public function create($nombre, $password)
+{
+    $rol = 'cliente'; //fijo
+    $query = "INSERT INTO {$this->table} (nombre, password, rol) VALUES (:nombre, :password, :rol)";
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(':nombre', $nombre);
+    $stmt->bindParam(':password', $password);
+    $stmt->bindParam(':rol', $rol);
+    return $stmt->execute();
+}
+   
 
-    public function create($nombre, $password, $rol)
-    {
-        $query = "INSERT INTO {$this->table} (nombre, password, rol) VALUES (:nombre, :password, :rol)";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(':nombre', $nombre);
-        $stmt->bindParam(':password', $password);
-        $stmt->bindParam(':rol', $rol);
-        return $stmt->execute();
-    }
-
-    public function update($idusuario, $nombre, $password, $rol)
-    {
-        $query = "UPDATE {$this->table} SET nombre = :nombre, password = :password, rol = :rol WHERE idusuario = :idusuario";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(':idusuario', $idusuario);
-        $stmt->bindParam(':nombre', $nombre);
-        $stmt->bindParam(':password', $password);
-        $stmt->bindParam(':rol', $rol);
-        return $stmt->execute();
-    }
-
-    public function delete($idusuario)
-    {
-        $query = "DELETE FROM {$this->table} WHERE idusuario = :idusuario";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(':idusuario', $idusuario);
-        return $stmt->execute();
-    }
 }
