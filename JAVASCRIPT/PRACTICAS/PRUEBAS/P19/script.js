ef62dcae36388b96fa1e56470f5935de8a2d5561
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-06-17 11:19:24 +0200

    EA

--- JAVASCRIPT/PRACTICAS/PRUEBAS/P19/script.js
+++ JAVASCRIPT/PRACTICAS/PRUEBAS/P19/script.js
@@ -3,34 +3,39 @@ fetch("datos.json")
     if(respuesta.ok) {
         return respuesta.json();
     } else {
-        throw new Error("Los datos llegaron fatal");
+        throw new Error("Fallo en la recepción de los datos");
     }
 })
 .then(miJSON => {
     let formulario = document.createElement("form");
+    let valor;
+    let etiqueta;
+    let entrada;
+    let salto;
 
     for(let clave in miJSON) {
-        let valor = miJSON[clave];
+        valor = miJSON[clave];
 
-        let etiqueta = document.createElement("label");
+        etiqueta = document.createElement("label");
+        entrada = document.createElement("input");
+        
         etiqueta.for = clave;
-        etiqueta.textContent = clave;
-        formulario.appendChild(etiqueta);
+        etiqueta.textContent = clave + ":";
 
-        let entrada = document.createElement("input");
         entrada.name = clave;
         entrada.value = valor;
-        formulario.appendChild(entrada);
 
-        let salto = document.createElement("br");
+        formulario.append(etiqueta, entrada);
+
+        salto = document.createElement("br");
         formulario.appendChild(salto);
+
+        document.body.appendChild(formulario);
     }
-    document.body.appendChild(formulario);
-    
 })
 .catch(error => {
-    console.error("Error: " + Error);
+    console.error("Error: ", error);
 })
 
-
 //LOS DOCUMENT DE CREATE ELEMENT!!!! LOS NOMBRE DE LAS VARIABLES!!!
+//APPENCHILD EL FORMULARIO!!!!!
